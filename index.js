218d12f0de9a96e7721ea4d84db0241c85ecac98
Krzysztof Lojniewski <kontakt@pagepro.pl>
2017-06-21 19:14:40 +0200

    class rewrite

--- index.js
+++ index.js
@@ -1,58 +1,61 @@
 const VOLUME_LIMIT = 1000
 const autobahn = require('autobahn')
-const MongoClient = require('mongodb').MongoClient
-const assert = require('assert');
+const Mongoose = require('mongoose');
+const TickerModel = require('./models/ticker')
 
 // Connection URL
 const DB_URL = 'mongodb://localhost:27017/poloaltbot';
-
+// POLONIEX URL
 const API_WS_URL = "wss://api.poloniex.com";
-var connection = new autobahn.Connection({
-  url: API_WS_URL,
-  realm: "realm1",
-  max_retries: -1
-});
 
-
-const listener = function (db) {
-  const tickersCollection = db.collection('tickers');
-
-  connection.onopen = function (session) {
-    function tickerEvent (args, kwargs) {
-      if (args[0].includes('BTC_') && Number(args[5]) > VOLUME_LIMIT) {
-        const tickerModel = {
-          currencyPair: args[0],
-          last: Number(args[1]),
-          lowestAsk: Number(args[2]),
-          highestBid: Number(args[3]),
-          percentChange: Number(args[4]),
-          baseVolume: Number(args[5]),
-          quoteVolume: Number(args[6]),
-          isFrozen: Number(args[7]),
-          dayHigh: Number(args[8]),
-          dayLow : Number(args[9]),
-          time: Math.floor(Date.now() / 1000)
-        }
-        tickersCollection.insertOne(tickerModel, function (error, r) {
-          if (error) {
-            console.error(error)
-            process.exit()
-          }
-        })
-      }
+class TickerLogger {
+  constructor () {
+    Mongoose.connect(DB_URL);
+    Mongoose.Promise = global.Promise
+  }
+  init () {
+    console.info("Init");
+    const connection = new autobahn.Connection({
+      url: API_WS_URL,
+      realm: "realm1",
+      max_retries: -1
+    });
+    connection.onopen = session => {
+      console.info("Websocket connection oppened");
+      session.subscribe('ticker', this.tickerEvent);
     }
-    session.subscribe('ticker', tickerEvent);
+    connection.onclose = () => {
+      console.info("Websocket connection closed");
+    }
+    connection.open();
   }
-
-  connection.onclose = function () {
-    console.log("Websocket connection closed");
+  tickerLogger (args, kwargs) {
+    console.log('tickerLogger')
+    if (args[0].includes('BTC_') && Number(args[5]) > VOLUME_LIMIT) {
+      const tickerUpdate = {
+        currencyPair: args[0],
+        last: Number(args[1]),
+        lowestAsk: Number(args[2]),
+        highestBid: Number(args[3]),
+        percentChange: Number(args[4]),
+        baseVolume: Number(args[5]),
+        quoteVolume: Number(args[6]),
+        isFrozen: Number(args[7]),
+        dayHigh: Number(args[8]),
+        dayLow : Number(args[9]),
+        time: Math.floor(Date.now() / 1000)
+      }
+      TickerModel(tickerUpdate).save(error => {
+        if (error) {
+          console.error(error)
+          process.exit()
+        } else {
+          console.info('created')
+        }
+      })
+    }
   }
-  connection.open();
 }
 
-MongoClient.connect(DB_URL, function(err, db) {
-  assert.equal(null, err);
-  console.log('Connected successfully to server');
-
-  listener(db)
-});
+const poloTicker = new TickerLogger()
+poloTicker.init()
