2bbc614c11723416bf7f0c3be62164afdfe4b886
Krzysztof Lojniewski <kontakt@pagepro.pl>
2017-06-21 19:46:48 +0200

    api rewritten

--- api.js
+++ api.js
@@ -1,55 +1,21 @@
+const Env = require('./config/env')
 const express = require('express')
 const app = express()
 
-const MongoClient = require('mongodb').MongoClient
-const assert = require('assert');
+const Mongoose = require('mongoose');
+const TickerModel = require('./models/ticker')
 
 // Connection URL
-const DB_URL = 'mongodb://localhost:27017/poloaltbot';
+Mongoose.connect(Env.DB_URL);
+Mongoose.Promise = global.Promise
 
-const WATCHED_PAIRS = [
-  'BTC_XMR',
-  'BTC_MAID',
-  'BTC_ETH',
-  'BTC_BTCD',
-  'BTC_BTS',
-  'BTC_DGB',
-  'BTC_DOGE',
-  'BTC_NMC',
-  'BTC_PPC',
-  'BTC_QORA',
-  'BTC_SILK',
-  'BTC_SYS',
-  'BTC_XRP',
-  'USDT_BTC']
-
-const listener = function (db) {
-  const tickersCollection = db.collection('tickers');
-
-  app.get('/', function (req, res) {
-    const lastRecord = tickersCollection.findOne({}, { sort:{$natural:-1}}, function (err, document) {
-      var newDate = new Date();
-      newDate.setTime(document.time * 1000);
-      dateString = newDate.toUTCString();
-      res.send(dateString)
-    })
+app.get('/', function (req, res) {
+  TickerModel.findOne().sort({$natural:-1}).then(ticker => {
+    const newDate = new Date()
+    newDate.setTime(ticker.time * 1000)
+    res.send(newDate.toUTCString())
   })
-  app.get('/stats', function (req, res) {
-    // max
-    // db.getCollection('tickers').find({"currencyPair": 'BTC_ETH'}).sort({ "highestBid": -1}).limit(1)
-    // min
-    // db.getCollection('tickers').find({"currencyPair": 'BTC_ETH'}).sort({ "highestBid": 1}).limit(1)
-    
-  })
-  app.listen(4000, function () {
-    console.log('Example app listening on port 4000!')
-  })
-}
-
-
-MongoClient.connect(DB_URL, function(err, db) {
-  assert.equal(null, err);
-  console.log('Connected successfully to server');
-
-  listener(db)
-});
+})
+app.listen(Env.API_PORT, function () {
+  console.log(`API Server listening on port ${Env.API_PORT}!`)
+})
